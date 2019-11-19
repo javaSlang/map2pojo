8d7765875cdd6d90b494732f3356e3985a8ab672
javaSlang <serhiylang@gmail.com>
2019-11-19 11:53:02 +0100

    #3. add core annotations: - done

---
+++ src/main/java/com/javaslang/map2pojo/annotations/Map2Pojo.java
@@ -0,0 +1,21 @@
+package com.javaslang.map2pojo.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public @interface Map2Pojo {
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @interface OrderedFields {
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
+    @interface FormattedDate {
+        String value();
+    }
+
+}
