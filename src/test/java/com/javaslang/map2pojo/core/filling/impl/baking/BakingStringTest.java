cc70a3dc3720a531c0c06973f04dc2b1c60943d8
javaSlang <serhiylang@gmail.com>
2019-11-20 16:42:47 +0100

    #17. add unit tests for baking functions - added 'BakingStringTest'

---
+++ src/test/java/com/javaslang/map2pojo/core/filling/impl/baking/BakingStringTest.java
@@ -0,0 +1,50 @@
+package com.javaslang.map2pojo.core.filling.impl.baking;
+
+import org.junit.Test;
+
+import static com.javaslang.map2pojo.core.filling.impl.baking.TestClass.TEST_CLASS_FIELDS;
+import static com.javaslang.map2pojo.core.filling.impl.baking.TestClass.TEST_FIELD;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNull;
+
+public class BakingStringTest {
+
+    private static final String EXPECTED = "testValue";
+
+    @Test
+    public void applyBasicTest() {
+        String testValue = new BakingString().apply(
+                TEST_CLASS_FIELDS.get(TEST_FIELD),
+                "testValue"
+        );
+        assertEquals(EXPECTED, testValue);
+    }
+
+    @Test
+    public void shouldBeTrimmedTest() {
+        String testValue = new BakingString().apply(
+                TEST_CLASS_FIELDS.get(TEST_FIELD),
+                "           testValue         "
+        );
+        assertEquals(EXPECTED, testValue);
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void nonStringValueTest() {
+        new BakingString().apply(
+                TEST_CLASS_FIELDS.get(TEST_FIELD),
+                new Object()
+        );
+    }
+
+    @Test
+    public void nullValueTest() {
+        assertNull(
+                new BakingString().apply(
+                        TEST_CLASS_FIELDS.get(TEST_FIELD),
+                        null
+                )
+        );
+    }
+
+}
