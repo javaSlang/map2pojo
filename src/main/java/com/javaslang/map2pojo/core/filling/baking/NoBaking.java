5ce8b0091912eec302a334c78d219ac58d0c47c5
javaSlang <serhiylang@gmail.com>
2019-11-20 09:51:02 +0100

    #11. revise the approach to 'Filling' implementations - added Fillings class impl

---
+++ src/main/java/com/javaslang/map2pojo/core/filling/baking/NoBaking.java
@@ -0,0 +1,22 @@
+package com.javaslang.map2pojo.core.filling.baking;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Field;
+import java.util.function.BiFunction;
+
+@Slf4j
+public class NoBaking<T> implements BiFunction<Field, Object, T> {
+
+    @Override
+    public T apply(Field field, Object o) {
+        throw new UnsupportedOperationException(
+                String.format(
+                        "There is no filling for %s type - field %s should be skipped",
+                        field.getType(),
+                        field.getName()
+                )
+        );
+    }
+
+}
