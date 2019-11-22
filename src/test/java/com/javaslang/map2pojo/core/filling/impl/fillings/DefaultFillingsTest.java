bae776271596353e932f04308dd71ad16e6ad343
javaSlang <serhiylang@gmail.com>
2019-11-22 10:54:15 +0100

    #27. add unit tests for fillings implementation - added unit tests for fillings

---
+++ src/test/java/com/javaslang/map2pojo/core/filling/impl/fillings/DefaultFillingsTest.java
@@ -0,0 +1,42 @@
+package com.javaslang.map2pojo.core.filling.impl.fillings;
+
+import com.javaslang.map2pojo.core.filling.impl.baking.BakingBigDecimal;
+import com.javaslang.map2pojo.core.filling.impl.baking.BakingString;
+import com.javaslang.map2pojo.core.filling.impl.filling.BasicFilling;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static com.javaslang.map2pojo.core.filling.impl.fillings.BasicFillingsTest.BASIC_FILLING_WITH_DATE_BAKING;
+import static com.javaslang.map2pojo.core.filling.impl.fillings.BasicFillingsTest.BASIC_FILLING_WITH_NO_BAKING;
+import static junit.framework.TestCase.assertEquals;
+
+public class DefaultFillingsTest {
+
+    @Test
+    public void defaultFillingsTest() {
+        DefaultFillings defaultFillings = new DefaultFillings();
+        assertEquals(
+                BASIC_FILLING_WITH_DATE_BAKING,
+                defaultFillings.appropriate(Date.class)
+        );
+        assertEquals(
+                new BasicFilling<>(
+                        new BakingBigDecimal()
+                ),
+                defaultFillings.appropriate(BigDecimal.class)
+        );
+        assertEquals(
+                new BasicFilling<>(
+                        new BakingString()
+                ),
+                defaultFillings.appropriate(String.class)
+        );
+        assertEquals(
+                BASIC_FILLING_WITH_NO_BAKING,
+                defaultFillings.appropriate(Object.class)
+        );
+    }
+
+}
