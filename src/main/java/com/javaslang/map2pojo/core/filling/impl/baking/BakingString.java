e0fcfa4c4a5ade7e9c80add8868e1af39542bde1
javaSlang <serhiylang@gmail.com>
2019-11-25 16:11:29 +0100

    #34. add support of date formatting for string fields - implementation

--- src/main/java/com/javaslang/map2pojo/core/filling/impl/baking/BakingString.java
+++ src/main/java/com/javaslang/map2pojo/core/filling/impl/baking/BakingString.java
@@ -1,8 +1,11 @@
 package com.javaslang.map2pojo.core.filling.impl.baking;
 
+import com.javaslang.map2pojo.annotations.Map2Pojo;
 import lombok.EqualsAndHashCode;
 
 import java.lang.reflect.Field;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.function.BiFunction;
 
 import static org.apache.commons.lang3.StringUtils.trim;
@@ -12,7 +15,16 @@ public class BakingString implements BiFunction<Field, Object, String> {
 
     @Override
     public String apply(Field field, Object rawValue) {
-        return trim((String) rawValue);
+        if (field.isAnnotationPresent(Map2Pojo.FormattedDate.class)) {
+            Map2Pojo.FormattedDate dateFormat = field.getAnnotation(Map2Pojo.FormattedDate.class);
+            return formattedDate((Date) rawValue, dateFormat.value());
+        } else {
+            return trim((String) rawValue);
+        }
+    }
+
+    private String formattedDate(Date rawValue, String format) {
+        return rawValue != null ? new SimpleDateFormat(format).format(rawValue) : null;
     }
 
 }
