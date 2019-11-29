36699c7eef5e9e964fd7eed5dc5ac9d3b114902c
javaSlang <serhiylang@gmail.com>
2019-11-29 14:24:00 +0100

    #53. refactoring for baking functions - add composite baking function - updated implementation

--- src/main/java/com/javaslang/map2pojo/core/filling/impl/baking/BakingString.java
+++ src/main/java/com/javaslang/map2pojo/core/filling/impl/baking/BakingString.java
@@ -2,29 +2,29 @@ package com.javaslang.map2pojo.core.filling.impl.baking;
 
 import com.javaslang.map2pojo.annotations.Map2Pojo;
 import com.javaslang.map2pojo.core.filling.BakingFunction;
-import com.javaslang.map2pojo.core.filling.impl.baking.exceptions.WrongTypeMappingException;
 import lombok.EqualsAndHashCode;
 
-import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 import static org.apache.commons.lang3.StringUtils.trim;
 
-@EqualsAndHashCode
-public class BakingString implements BakingFunction<String> {
+@EqualsAndHashCode(callSuper = false)
+public class BakingString extends CompositeBakingFunction<String> {
 
-    @Override
-    public String apply(Field field, Object rawValue) {
-        String bakedString;
-        if (field.isAnnotationPresent(Map2Pojo.FormattedDate.class)) {
-            Map2Pojo.FormattedDate dateFormat = field.getAnnotation(Map2Pojo.FormattedDate.class);
-            return new SimpleDateFormat(dateFormat.value()).format(rawValue);
-        } else if (rawValue instanceof String) {
-            bakedString = trim((String) rawValue);
-        } else {
-            throw new WrongTypeMappingException(rawValue, String.class);
-        }
-        return bakedString;
+    public BakingString() {
+        super(
+                new HashMap<Class, BakingFunction<String>>() {
+                    {
+                        put(String.class, (field, rawValue) -> trim((String) rawValue));
+                        put(Date.class, (field, rawValue) -> {
+                            Map2Pojo.FormattedDate dateFormat = field.getAnnotation(Map2Pojo.FormattedDate.class);
+                            return new SimpleDateFormat(dateFormat.value()).format(rawValue);
+                        });
+                    }
+                }
+        );
     }
 
 }
