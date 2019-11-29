90955ad3c8a553ac02c1a7dae467c4318e7fe0e0
javaSlang <serhiylang@gmail.com>
2019-11-29 10:19:35 +0100

    #51. fix baking of empty strings - updated implementation

--- src/main/java/com/javaslang/map2pojo/core/filling/BakingFunction.java
+++ src/main/java/com/javaslang/map2pojo/core/filling/BakingFunction.java
@@ -3,10 +3,12 @@ package com.javaslang.map2pojo.core.filling;
 import java.lang.reflect.Field;
 import java.util.function.BiFunction;
 
+import static org.springframework.util.StringUtils.isEmpty;
+
 public interface BakingFunction<T> extends BiFunction<Field, Object, T> {
 
     default T bake(Field field, Object rawValue) {
-        return rawValue == null ? null : apply(field, rawValue);
+        return isEmpty(rawValue) ? null : apply(field, rawValue);
     }
 
 }
