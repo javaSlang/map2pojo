01e39ea1a5c05a10bb4ea7507275f16bf241663f
javaSlang <serhiylang@gmail.com>
2019-12-11 23:34:16 +0100

    #72. add support for Timestamp to LocalDateTime - implemented

--- src/main/java/com/javaslang/map2pojo/core/filling/impl/baking/time/BakingLocalDateTime.java
+++ src/main/java/com/javaslang/map2pojo/core/filling/impl/baking/time/BakingLocalDateTime.java
@@ -3,6 +3,7 @@ package com.javaslang.map2pojo.core.filling.impl.baking.time;
 import com.javaslang.map2pojo.core.filling.impl.baking.CompositeBakingFunction;
 import com.javaslang.map2pojo.core.filling.impl.baking.conversions.BasicConversions;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 public class BakingLocalDateTime extends CompositeBakingFunction<LocalDateTime> {
@@ -11,6 +12,7 @@ public class BakingLocalDateTime extends CompositeBakingFunction<LocalDateTime> 
         super(
                 new BasicConversions<LocalDateTime>()
                         .with(String.class, new BakingLocalDate.StringToTemporalConversion<>(LocalDateTime::parse))
+                        .with(Timestamp.class, (field, rawValue) -> rawValue.toLocalDateTime())
                         .with(LocalDateTime.class, (field, rawValue) -> rawValue)
         );
     }
